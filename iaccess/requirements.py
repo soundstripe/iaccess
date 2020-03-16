a75ab0385f866c3c5f4c9646b2e5c8097b3abad1
Steven James <sjames@jacksonfurnind.com>
2020-03-16 12:58:13 -0400

    workaround strange attributeerrors

--- iaccess/requirements.py
+++ iaccess/requirements.py
@@ -1,5 +1,18 @@
+from sqlalchemy.testing import exclusions
 from sqlalchemy.testing.requirements import SuiteRequirements
 
 
 class Requirements(SuiteRequirements):
-    pass
+    @property
+    def order_by_col_from_union(self):
+        """target database supports ordering by a column from a SELECT
+        inside of a UNION
+
+        E.g.  (SELECT id, ...) UNION (SELECT id, ...) ORDER BY id
+
+        """
+        return exclusions.open()
+
+    @property
+    def cross_schema_fk_reflection(self):
+        return exclusions.closed()
