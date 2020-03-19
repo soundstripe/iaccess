6e5ecb1980caf678bfa0fdb7c5b720071bd69be1
Steven James <sjames@jacksonfurnind.com>
2020-03-19 15:33:59 -0400

    implement a way to mark some tests skipped

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,4 +1,12 @@
 # test/test_suite.py
 
 # noinspection PyUnresolvedReferences
+import pytest
 from sqlalchemy.testing.suite import *
+
+
+class ComponentReflectionTest(ComponentReflectionTest):
+    @pytest.mark.skip(reason="unique constraints with duplicate column sets unsupported on db2 for iseries")
+    def test_get_unique_constraints(self):
+        return super().test_get_unique_constraints()
+
