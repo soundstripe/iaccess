aecf8393d4653e1b077bbbf02f6559281fff8bb6
Steven James <sjames@jacksonfurnind.com>
2020-04-01 19:36:35 -0400

    fix travis pypi deployment

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/soundstripe/iaccess',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
     entry_points={
      'sqlalchemy.dialects': [
