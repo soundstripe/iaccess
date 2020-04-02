77e11800b7143997308ac9182b84686c43525ce9
Steven James <sjames@jacksonfurnind.com>
2020-04-02 12:26:50 -0400

    warn when i Access driver is not found by pyodbc

--- iaccess/connector.py
+++ iaccess/connector.py
@@ -1,12 +1,28 @@
 import re
+import warnings
 
+import pyodbc
 from sqlalchemy.connectors.pyodbc import PyODBCConnector
+
+CLIENT_SOLUTIONS_URL = 'https://www.ibm.com/support/pages/odbc-driver-ibm-i-access-client-solutions'
+
+
+class IAccessDriverMissingWarning(UserWarning):
+    pass
 
 
 class IAccessConnector(PyODBCConnector):
     pyodbc_driver_name = 'IBM i Access ODBC Driver'
 
     def create_connect_args(self, url):
+        available_drivers = pyodbc.drivers()
+        selected_driver = url.query.get('DRIVER', self.pyodbc_driver_name)
+        if selected_driver not in available_drivers:
+            warnings.warn(f'driver "{selected_driver}" not found by pyodbc\n'
+                          f'install from {CLIENT_SOLUTIONS_URL} or specify an alternate driver name\n'
+                          f'available drivers: {repr(available_drivers or None)}',
+                          IAccessDriverMissingWarning)
+
         connectors, connect_args = super().create_connect_args(url)
 
         # force unicode mode (utf-16 instead of ebdic default)
